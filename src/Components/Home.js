d88b76bca93c3d450c8c6d231bb6341c3fba4d28
pradeep7878 <pradivpj@gmail.com>
2023-09-09 12:04:31 +0530

    bgcolor changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import Same from '../Components/Same';
 const Home = () => {
     return (
         <>
-            <h1 style={{textAlign:'center',backgroundColor:'#91fcea',padding:'1rem',margin:'0'}}>Welcome to CLIKTECH</h1>
+            <h1 style={{textAlign:'center',background:'linear-gradient(to right,#20f1ce,#b7f7ec,#20f1ce)',padding:'1rem',margin:'0'}}>Welcome to CLIKTECH</h1>
             <Same
                 title='Learning Web Development'
                 text='Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).[1] Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services. A more comprehensive list of tasks to which Web development commonly refers, may include Web engineering, Web design, Web content development, client liaison, client-side/server-side scripting, Web server and network security configuration, and e-commerce development.'
